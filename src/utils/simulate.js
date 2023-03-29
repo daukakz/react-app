7cfe928852b6fb2d1d3c7241a6d6f056046f3e2e
Daulet Dosmuratov <ddosmuratov@gmail.com>
2023-03-29 18:57:37 +0600

    function-for-fight

---
+++ src/utils/simulate.js
@@ -0,0 +1,7 @@
+const legion = [{name: "Ancient Appariton"}, {name: "Muerta"}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
+
+const duel = (hero1, hero2) => { 
+  return hero1.name
+}
+
+duel(legion[0], legion[1])
