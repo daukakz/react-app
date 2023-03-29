b997eaffbfbda0626f8f586873b99a1d7fc16513
Daulet Dosmuratov <ddosmuratov@gmail.com>
2023-03-29 19:18:37 +0600

    duel-heroes

--- src/utils/simulate.js
+++ src/utils/simulate.js
@@ -1,7 +1,11 @@
-const legion = [{name: "Ancient Appariton"}, {name: "Muerta"}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
+const legion = [{name: "Ancient Appariton"}, {name: "Muerta"}, {name: "Primal Beast"}, {name: "Quenn of Pain"}, {name: "Lina"}, {name: "Windranger"}, {name: "Mars"}, {}, {}, {}, {}, {}];
 
 const duel = (hero1, hero2) => { 
-  return hero1.name
+  if(Math.random()*10 < 5) { 
+    return hero1.name
+  } else {
+    return hero2.name
+  }
 }
 
 duel(legion[0], legion[1])
